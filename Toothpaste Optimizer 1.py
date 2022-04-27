fb577b1cfd2e1691cf4a72df6f401c650101ee98
Cheoddox <leeroywycoff@gmail.com>
2022-04-27 13:52:39 -0500

    Update Toothpaste Optimizer 1.py

--- Toothpaste Optimizer 1.py
+++ Toothpaste Optimizer 1.py
@@ -1,17 +1,19 @@
 
+from os import system
 from tkinter import *
-from time import *
-from datetime import datetime
+import time
 
-now = datetime.now()
-current_time = now.strftime("%I:%M")
-print(current_time)
+WIDTH = 400
+HEIGHT = 400
+
+def update_clock():
+    timer_label.config(text = time.strftime("%I:%M:%S", time.localtime()))
+    window.after(1000, update_clock)
 
 GUI_data = [
-    {"row": 0, "col": 0, "colspan": 3, "value": ""},
-    {"row": 0, "col": 3, "colspan": 3, "value": ""},
-    {"row": 1, "col": 0, "colspan": 2, "value": ""},
-    {"row": 1, "col": 5, "colspan": 1, "value": current_time}
+{"row": 0, "col": 0, "colspan": 3, "value": ""},
+{"row": 0, "col": 3, "colspan": 3, "value": ""},
+{"row": 1, "col": 0, "colspan": 2, "value": ""},
 ]
 
 USING_RPI = False
@@ -24,10 +26,30 @@ class MainGUI(Frame):
             master.attributes("-fullscreen", True)
         self.setupGUI()
 
-    def setupGUI():
-        pass
+    def make_GUI_item(self, row, col, colspan, value):
 
-#window = Tk()
-#window.title("Toothpaste Optimization Prototype")
+        GUI_item = Label(self, text=value, anchor=CENTER, bg="white", fg="black", height=1)
 
-#window.mainloop()
+        GUI_item.grid(row=row, column=col, columnspan=colspan, sticky=NSEW)
+
+    def setupGUI(self):
+
+        for row in range(2):
+            Grid.rowconfigure(self, row, weight = 1)
+        
+        for col in range(6):
+            Grid.columnconfigure(self, col, weight = 1)
+
+        for GUI_item in GUI_data:
+            self.make_GUI_item(GUI_item["row"], GUI_item["col"], GUI_item["colspan"], GUI_item["value"])
+
+        self.pack(fill = BOTH, expand = 1)
+
+window = Tk()
+gui = MainGUI(window)
+window.geometry(f"{WIDTH}x{HEIGHT}")
+window.title("Terrific Toothpaste Prototype")
+timer_label = Label(window, text = '', anchor=SE, bg="white", fg="black", height = 1)
+timer_label.pack(fill=BOTH, expand=1)
+window.after(0, update_clock)
+window.mainloop()
